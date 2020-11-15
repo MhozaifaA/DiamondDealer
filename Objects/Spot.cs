f3eab6f33ac4e8dca37e976e704e1f2bb3c11466
MHozaifaA <botchey.2012@gmail.com>
2020-11-15 22:44:58 +0200

    Add project files.

---
+++ Objects/Spot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiamondDealer.Objects
+{
+    public class Spot
+    {
+        public SpotTypes SpotTypes { get; set; }
+        public string UrlImage { get; set; }
+        public bool IsCurrentPostion { get; set; }
+    }
+}
